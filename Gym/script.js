334e1f7fdcaebf97906245cfd9fe1a446da8c3f1
Sbelgana <44445774+Sbelgana@users.noreply.github.com>
2025-07-31 00:05:15 -0400

    Update script.js

--- Gym/script.js
+++ Gym/script.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     DEFAULT_WEBHOOK: "",
     DEFAULT_API_KEY: "",
-    DEFAULT_CSS: [        'https://cdn.jsdelivr.net/gh/Sbelgana/AI_NextGen@c44de80/FormFields.css'],
+    DEFAULT_CSS: [        'https://cdn.jsdelivr.net/gh/Sbelgana/AI_NextGen@4e75b7a/FormFields.css'],
     SESSION_TIMEOUT: 900000, // 15 minutes
     SESSION_WARNING: 780000, // 13 minutes
     DEBOUNCE_DELAY: 50,
